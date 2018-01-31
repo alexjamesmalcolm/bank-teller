bb11f64801628a76fe9159ad11760009ef81d23c
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-01-31 15:04:04 -0500

    Created and solved test shouldGetBankAccount

---
+++ src/main/java/bankteller/Bank.java
@@ -0,0 +1,18 @@
+package bankteller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bank {
+	//Map<String, Double> menu = new HashMap<String, Double>();
+	Map<String, BankAccount> accounts = new HashMap<>();
+
+	public void addAccount(BankAccount account) {
+		accounts.put(account.getAccountNum(), account);
+	}
+
+	public BankAccount getAccount(String string) {
+		return accounts.get(string);
+	}
+
+}
--- src/test/java/bankteller/BankTest.java
+++ src/test/java/bankteller/BankTest.java
@@ -1,5 +1,16 @@
 package bankteller;
 
-public class BankTest {
+import org.junit.Test;
+import org.junit.Assert;
 
+public class BankTest {
+	
+	@Test
+	public void shouldGetBankAccount() {
+		BankAccount account = new BankAccount("0001", "checking", 100.0);
+		Bank underTest = new Bank();
+		underTest.addAccount(account);
+		BankAccount returnedAccount = underTest.getAccount("0001");
+		Assert.assertEquals(account, returnedAccount);
+	}
 }
