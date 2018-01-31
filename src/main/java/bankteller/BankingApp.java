73908efb193bf4a700967e2c63732c45ad6c77fc
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-01-31 17:29:24 -0500

    Finished menu for app

--- src/main/java/bankteller/BankingApp.java
+++ src/main/java/bankteller/BankingApp.java
@@ -21,31 +21,43 @@ public class BankingApp {
 		int choice = input.nextInt();
 		if (choice == 1) {
 			String accountNum = askAccount(myBank, input);
-			System.out.println("Enter the amount to deposit:");
-			double amount = input.nextDouble();
-			myBank.deposit(accountNum, amount);
-			double balance = myBank.getAccount(accountNum).getBalance();
-			System.out.println("Your updated balance is: " + balance);
+			askToDeposit(myBank, input, accountNum);
+			displayAccountBalance(myBank, accountNum);
 		} else if (choice == 2) {
 			String accountNum = askAccount(myBank, input);
-			System.out.println("Enter the amount to withdrawal:");
-			double amount = input.nextDouble();
-			myBank.withdraw(accountNum, amount);
-			double balance = myBank.getAccount(accountNum).getBalance();
-			System.out.println("Your updated balance is: " + balance);
+			askToWithdraw(myBank, input, accountNum);
+			displayAccountBalance(myBank, accountNum);
 		} else if (choice == 3) {
 			String accountNum = askAccount(myBank, input);
-			double balance = myBank.getAccount(accountNum)
+			displayAccountBalance(myBank, accountNum);
 		} else if (choice == 4) {
-			// close an account
+			String accountNum = askAccount(myBank, input);
+			myBank.closeAccount(accountNum);
 		} else if (choice == 5) {
-			// exit
+			System.exit(0);
 		}
 		input.close();
 	}
 
-	private static String askAccount(Bank myBank, Scanner input) {
-		myBank.displayAccounts();
+	private static void askToDeposit(Bank myBank, Scanner input, String accountNum) {
+		System.out.println("Enter the amount to deposit:");
+		double amount = input.nextDouble();
+		myBank.deposit(accountNum, amount);
+	}
+
+	private static void askToWithdraw(Bank bank, Scanner input, String accountNum) {
+		System.out.println("Enter the amount to withdrawal:");
+		double amount = input.nextDouble();
+		bank.withdraw(accountNum, amount);
+	}
+
+	private static void displayAccountBalance(Bank bank, String accountNum) {
+		double balance = bank.getBalance(accountNum);
+		System.out.println("Your balance is: " + balance);
+	}
+
+	private static String askAccount(Bank bank, Scanner input) {
+		bank.displayAccounts();
 		System.out.println("Select the account by (acct num) to perform this transaction.");
 		String accountNum = input.next();
 		System.out.println("You have selected " + accountNum);
